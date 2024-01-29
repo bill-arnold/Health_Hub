67024127757f0e88f81c05f0b3aeb2e3935a086d
bill_arnold <bill.arnold@student.moringschool.com>
2024-01-29 07:53:07 +0300

    new additions

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
 from user_resource import UserResource, UsersResource, UserLoginResource
@@ -7,27 +7,31 @@ from doctor_resource import DoctorResource, DoctorsResource
 from patient_resource import PatientResource, PatientsResource
 from disease_resource import DiseaseResource, DiseasesResource
 from appointment_resource import AppointmentResource, AppointmentsResource
-from symptom_resource import SymptomResource, SymptomsResource  # Added import
+from symptom_resource import SymptomResource, SymptomsResource
 from models import db, User, Authorization, Doctor, Patient, Symptom, Disease, Appointment
 from flask_migrate import Migrate
-from dotenv import load_dotenv  # Corrected import
+from dotenv import load_dotenv
 import os
 from flask_cors import CORS
 
-app = Flask(__name__)
+app = Flask(
+    __name__,
+    static_url_path='/static',
+    static_folder='client/my-app/dist',
+    template_folder='client/my-app/dist'
+)
 
 # Load environment variables from .env file
 load_dotenv()
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///health_database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY','default_secret_key' )
+app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY', 'default_secret_key')
 jwt = JWTManager(app)
 db.init_app(app)
 migrate = Migrate(app, db)
 api = Api(app)
 CORS(app, origins=["*"])
-#CORS(app, resources={r"/api/*": {"origins": "http://127.0.0.1:5173"}})
 
 # Add API resources here using the resource classes
 api.add_resource(UserResource, '/user/<int:user_id>')
@@ -52,5 +56,12 @@ api.add_resource(DiseasesResource, '/diseases')
 api.add_resource(AppointmentResource, '/appointment/<int:appointment_id>')
 api.add_resource(AppointmentsResource, '/appointments')
 
+
+# Serve the index.html file
+@app.route('/')
+@app.route('/<int:id>')
+def index(id=0):
+    return ("index.html")
+
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5555,debug=True)
+    app.run()
